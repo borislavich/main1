235e5327b1f87bf572ca26bf24c6be38c539f049
borislavich <menenado2@gmail.com>
2024-10-19 23:37:41 +0300

    Add files via upload

---
+++ main25.py
@@ -0,0 +1,15 @@
+from cgi import print_environ_usage
+
+print('hello')
+print(12+89-1)
+##1st program
+print((9**0.5)*5)
+##2nd program
+print(9.99>9.98 and 1000!=1000.1)
+##3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==2*(2+2))
+##4th program
+print('123.456')
+print(int(float('123.456')*10)%10)
